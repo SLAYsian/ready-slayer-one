12def30b0467986ed67f9c34553ae6770be528e4
Aidan <farinaaidan2@gmail.com>
2023-06-29 21:40:22 -0400

    did work on the profile and got the quests to show up

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Model } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const { User, Outcome, } = require('../models');
 const withAuth = require('../utils/auth');
 
@@ -30,15 +30,17 @@ router.get('/profile', withAuth, async (req, res) => {
     });
 
     const user = userData.get({ plain: true });
-    console.log('PROFILE LOG: ', JSON.stringify(user, null, 2));
-    // const userOutcomes = await Outcome.findAll({
-    //   where: { user_id: req.session.user_id },
-    //   attributes: ['name', 'description']
-    // });
-
+    // console.log('PROFILE LOG: ', JSON.stringify(user, null, 2));
+    let userOutcomes = await Outcome.findAll({
+      where: { user_id: req.session.user_id },
+      attributes: ['name', 'description']
+    });
+    userOutcomes = userOutcomes.map((outcome) => outcome.get({ plain: true }));
+    console.log('User:', user);
+    console.log('User Outcomes:', userOutcomes);
     res.render('profile', {
       ...user,
-      // userOutcomes,
+      userOutcomes,
       logged_in: true
     });
   } catch (err) {
